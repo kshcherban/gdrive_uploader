4d20dd566d406b01f0e404e6b313a0c56f952576
Konstantin Shcherban <k.scherban@gmail.com>
2012-12-30 12:31:12 +0800

    Rewrote prints to be compatible with Python 3 and added input checks

--- gdrive_upload.py
+++ gdrive_upload.py
@@ -26,11 +26,11 @@ file_path = sys.argv[1]
 token_file = sys.path[0] + '/auth_token.txt'
 
 # Copy your credentials from the APIs Console
-CLIENT_ID = '359672913375-66u0je9n6apvq28n49433jqug793drjm.apps.googleusercontent.com'
-CLIENT_SECRET = 'Tjap_lkL5jatMv4MkeDWBLyO'
+CLIENT_ID = 'YOUR CLIENT ID'
+CLIENT_SECRET = 'YOUR CLIENT SECRET'
 # Check https://developers.google.com/drive/scopes for all available scopes
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive.file'
-# Redirect URI for installed apps
+# Redirect URI for installed apps, can be left as is
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 
@@ -50,7 +50,7 @@ def create_token_file(token_file):
         REDIRECT_URI
         )
     authorize_url = flow.step1_get_authorize_url()
-    print 'Go to the following link in your browser: ' + authorize_url
+    print('Go to the following link in your browser: ' + authorize_url)
     code = raw_input('Enter verification code: ').strip()
     credentials = flow.step2_exchange(code)
     storage = Storage(token_file)
@@ -83,6 +83,7 @@ def upload_file(file_path, file_name, mime_type):
       'mimeType': mime_type,
     }
 # Permissions body description: anyone who has link can upload
+# Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
     permissions = {
       'role': 'reader',
       'type': 'anyone',
@@ -91,7 +92,7 @@ def upload_file(file_path, file_name, mime_type):
     }
 # Insert a file
     file = drive_service.files().insert(body=body, media_body=media_body).execute()
-# Inserting new permissions
+# Insert new permissions
     drive_service.permissions().insert(fileId=file['id'], body=permissions).execute()
 # Define file instance and get url for download
     file = drive_service.files().get(fileId=file['id']).execute()
@@ -101,6 +102,15 @@ def upload_file(file_path, file_name, mime_type):
 
 
 if __name__ == '__main__':
+# Check if file provied as argument and exists
+    if len(sys.argv) != 2:
+        print("One file should be provided as argument")
+        sys.exit(1)
+    try:
+        with open(token_file) as f: pass
+    except IOError as e:
+        print(e)
+        sys.exit(1):
 # Check if token file exists, if not create it by requesting authorization code
     try:
         with open(token_file) as f: pass
@@ -111,9 +121,9 @@ if __name__ == '__main__':
     file_name, mime_type = file_ops(file_path)
 # Sometimes API fails to retrieve starting URI, we wrap it.
     try:
-        print upload_file(file_path, file_name, mime_type)
+        print(upload_file(file_path, file_name, mime_type))
     except apiclient.errors.ResumableUploadError as e:
-        print "Error occured while first upload try:", e
-        print "Trying one more time."
-        print upload_file(file_path, file_name, mime_type)
+        print("Error occured while first upload try:", e)
+        print("Trying one more time.")
+        print(upload_file(file_path, file_name, mime_type))
 
