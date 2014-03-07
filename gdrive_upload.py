7599456ea8236e1f8d2093fa0f4440e7a7fc6f90
kshcherban <k.scherban@gmail.com>
2014-03-07 16:31:30 +0400

    Corrected except error, fixed none mime type

--- gdrive_upload.py
+++ gdrive_upload.py
@@ -13,6 +13,7 @@ from mimetypes import guess_type
 # sudo pip install --upgrade google-api-python-client
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
+from apiclient.errors import ResumableUploadError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 
@@ -35,6 +36,7 @@ REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 # Get mime type and name of given file
 def file_ops(file_path):
     mime_type = guess_type(file_path)[0]
+    mime_type = mime_type if mime_type else 'text/plain'
     file_name = file_path.split('/')[-1]
     return file_name, mime_type
 
@@ -42,8 +44,8 @@ def file_ops(file_path):
 def create_token_file(token_file):
 # Run through the OAuth flow and retrieve credentials
     flow = OAuth2WebServerFlow(
-        CLIENT_ID, 
-        CLIENT_SECRET, 
+        CLIENT_ID,
+        CLIENT_SECRET,
         OAUTH_SCOPE,
         REDIRECT_URI
         )
@@ -76,17 +78,17 @@ def upload_file(file_path, file_name, mime_type):
                                  mimetype=mime_type,
                                  resumable=True)
     body = {
-      'title': file_name,
-      'description': 'backup',
-      'mimeType': mime_type,
+        'title': file_name,
+        'description': 'backup',
+        'mimeType': mime_type,
     }
 # Permissions body description: anyone who has link can upload
 # Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
     permissions = {
-      'role': 'reader',
-      'type': 'anyone',
-      'value': None,
-      'withLink': True
+        'role': 'reader',
+        'type': 'anyone',
+        'value': None,
+        'withLink': True
     }
 # Insert a file
     file = drive_service.files().insert(body=body, media_body=media_body).execute()
@@ -96,8 +98,6 @@ def upload_file(file_path, file_name, mime_type):
     file = drive_service.files().get(fileId=file['id']).execute()
     download_url = file.get('webContentLink')
     return download_url
-
-
 
 if __name__ == '__main__':
 # Check if file provied as argument and exists
@@ -123,8 +123,7 @@ if __name__ == '__main__':
 # Sometimes API fails to retrieve starting URI, we wrap it.
     try:
         print(upload_file(file_path, file_name, mime_type))
-    except apiclient.errors.ResumableUploadError as e:
+    except ResumableUploadError as e:
         print("Error occured while first upload try:", e)
         print("Trying one more time.")
         print(upload_file(file_path, file_name, mime_type))
-
